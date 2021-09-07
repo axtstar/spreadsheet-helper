0d09f3b366a483c51305f6f6d478ba91a9a6efdc
axt★ <axtstart@gmail.com>
2021-09-07 23:59:36 +0900

    fix

--- src/Code.ts
+++ src/Code.ts
@@ -11,17 +11,18 @@ class Column {
     this.validation = validation;
   }
 
-  getValue(value: Number | String | Date | Boolean) {
+  getValue(value: Number | String | Date | Boolean | null) {
     if (this.type === 'number' || this.type === 'bool') {
       if (value === null) {
         return '';
       } else {
-        return `${(<String>value).replace(/,/g, '').replace(/"/g, '""')}`;
+        return `${String(value).replace(/,/g, '').replace(/"/g, '""')}`;
       }
     } else if (this.type === 'date') {
-      if (value !== null && value !== '') {
-        const dt = new Date('Dec 30, 1899 00:00:00');
-        dt.setDate(dt.getDate() + Number(value));
+      if (value !== null) {
+        if (!(value instanceof Date)) return '""'
+        //const dt = new Date('Dec 30, 1899 00:00:00');
+        const dt = value
         const dateStr = `"${dt.getFullYear()}-${
           dt.getMonth() + 1
         }-${dt.getDate()}"`;
@@ -30,19 +31,19 @@ class Column {
         if (value === null) {
           return '""';
         } else {
-          return `"${(<String>value).replace(/"/g, '""')}"`;
+          return `"${String(value).replace(/"/g, '""')}"`;
         }
       }
     } else {
       if (value === null) {
         return '""';
       } else {
-        return `"${(<String>value).replace(/"/g, '""')}"`;
+        return `"${String(value).replace(/"/g, '""')}"`;
       }
     }
   }
 
-  isEmpty(value) {
+  isEmpty(value: any) {
     if (value === '') {
       return this.name + 'は必須です。';
     } else {
@@ -59,9 +60,9 @@ const regExp_requiredNumericRange = new RegExp(
 const regExp_requiredRegexp = new RegExp('requiredRegexp\\((.+)\\)');
 
 export class Columns {
-  columns: Array<Column>;
-  constructor(columnSheet) {
-    const data = columnSheet.getDataRange().getDisplayValues();
+  columns: Array<Column> = [];
+  constructor(columnSheet: GoogleAppsScript.Spreadsheet.Sheet) {
+    const data = columnSheet.getDataRange().getValues();
     if (data.length > 1) {
       this.columns = [];
       data.forEach(row => {
@@ -76,7 +77,7 @@ export class Columns {
   }
 
   //emptyチェック
-  requireNotNull(value, name) {
+  requireNotNull(value: any, name: String) {
     if (value === '') {
       return name + 'は必須です。\n';
     } else {
@@ -84,7 +85,7 @@ export class Columns {
     }
   }
 
-  requireStringSize(value, size, name) {
+  requireStringSize(value: any, size: Number, name: String) {
     let result = '';
     if (value.length > Number(size)) {
       result = name + 'は' + size + '文字以内です。(' + value.length + ')\n';
@@ -94,7 +95,7 @@ export class Columns {
     return result;
   }
 
-  numericRange(value, lower, higher, name) {
+  numericRange(value: any, lower: Number, higher: Number, name: String) {
     let result = '';
     if (value === '') {
       result = '';
@@ -107,7 +108,7 @@ export class Columns {
     return result;
   }
 
-  requireRegEx(value, regex, name) {
+  requireRegEx(value: any, regex: string | RegExp, name: String) {
     let result = '';
     const reg = new RegExp(regex);
     const r = value.match(reg);
@@ -118,7 +119,7 @@ export class Columns {
     return result;
   }
 
-  validate(row, rowOffset) {
+  validate(row: any[], rowOffset: any) {
     let result = '';
     for (let i = 0; i < row.length; i++) {
       const c = this.getColumn(i);
@@ -135,19 +136,19 @@ export class Columns {
             }
             if (r2 !== null) {
               result =
-                result + this.requireStringSize(`${row[i]}`, r2[1], c.name);
+                result + this.requireStringSize(`${row[i]}`, Number(r2[1]), c.name);
             }
             if (r3 !== null) {
               result =
-                result + this.numericRange(`${row[i]}`, r3[1], r3[2], c.name);
+                result + this.numericRange(`${row[i]}`, Number(r3[1]), Number(r3[2]), c.name);
             }
             if (r4 !== null) {
               result = result + this.requireRegEx(`${row[i]}`, r4[1], c.name);
             }
 
-            console.log(
-              `${i} ${c.name} ${v} ${row[i]} : ${r1} ${r2} ${r3} >>> ${result}`
-            );
+            // console.log(
+            //   `${i} ${c.name} ${v} ${row[i]} : ${r1} ${r2} ${r3} >>> ${result}`
+            // );
           }
         });
       }
@@ -155,7 +156,7 @@ export class Columns {
     return result;
   }
 
-  getColumn(id) {
+  getColumn(id: Number) {
     let result = null;
     for (let i = 0; i < this.columns.length; i++) {
       if (this.columns[i].id === id) {
@@ -166,12 +167,12 @@ export class Columns {
     return result;
   }
 
-  getLine(row, columnOffset) {
+  getLine(row: any, columnOffset: number) {
     const cols = [];
     for (let i = 0; i < this.columns.length; i++) {
       const c = this.columns[i];
       //console.log(`${c.id} ${c.type} ${c.name}`)
-      const location = c.id + columnOffset + 1;
+      const location = Number(c.id) + columnOffset + 1;
       if (location > row.length) {
         cols.push(c.getValue(null));
       } else {
@@ -181,7 +182,7 @@ export class Columns {
     return cols;
   }
 
-  isRecordEmpty(row): Boolean {
+  isRecordEmpty(row: any[]): Boolean {
     let result = true;
     row.forEach(col => {
       result = result && col === null;
@@ -191,18 +192,18 @@ export class Columns {
   }
 
   convertRangeToCsvFile(
-    sheet,
-    rowOffset,
-    columnOffset,
+    sheet: GoogleAppsScript.Spreadsheet.Sheet,
+    rowOffset: number,
+    columnOffset: number,
     _minRow: number,
     _maxRow: number
   ): String {
     try {
       const minRow = _minRow - 1;
       const maxRow = _maxRow - 1;
-      const data = sheet.getDataRange().getDisplayValues();
+      const data = sheet.getDataRange().getValues();
       if (data.length > 1) {
-        const rows = [];
+        const rows: any[] = [];
         let rowIndex = 0;
         data.forEach(row => {
           const cols = this.getLine(row, columnOffset);
@@ -216,9 +217,9 @@ export class Columns {
             //先頭の必須が空の行は追加しない
             const headCol = cols[0];
             if (!this.isRecordEmpty(cols)) {
-              console.log(`add ${rowIndex} ${cols.length} ${headCol}`);
+              //console.log(`add ${rowIndex} ${cols.length} ${headCol}`);
               const line = cols.join(',');
-              console.log(line);
+              //console.log(line);
               rows.push(line);
             } else {
               //範囲外スキップ
@@ -231,8 +232,9 @@ export class Columns {
       }
     } catch (err) {
       Logger.log(err);
-      Browser.msgBox(err);
+      Browser.msgBox(String(err));
     }
+    return ""
   }
 }
 
@@ -248,23 +250,26 @@ function adCheckTest() {
 
   //settings
   const ss: GoogleAppsScript.Spreadsheet.Spreadsheet | null = SpreadsheetApp.getActiveSpreadsheet();
-  readConfig();
-  console.log(`rowOffset: ${rowOffset}`);
-  console.log(`columnOffset : ${columnOffset}`);
+  if (ss === null) return
+
+  readConfig()
+  // console.log(`rowOffset: ${rowOffset}`)
+  // console.log(`columnOffset : ${columnOffset}`)
 
   //column読み込み
   if (ss !== null) {
-    const sheetColumn = ss.getSheetByName(columnsSheet);
+    const sheetColumn: GoogleAppsScript.Spreadsheet.Sheet | null = ss.getSheetByName(columnsSheet)
+    if (sheetColumn === null) return
     const cols = new Columns(sheetColumn);
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const value: any = (<GoogleAppsScript.Spreadsheet.Spreadsheet>ss)
-      .getActiveRange()
+
+    const value: any = ss
+      .getActiveRange()!
       .getValues()[0];
     const result = cols.validate(value, 0);
-    console.log(`result: ${result}`);
+    // console.log(`result: ${result}`);
     return result;
   } else {
-    console.log('result: sheet not found');
+    // console.log('result: sheet not found');
     return 'result: sheet not found';
   }
 }
@@ -283,18 +288,21 @@ function record_check(
     //settings
     const ss = SpreadsheetApp.getActiveSpreadsheet();
     readConfig();
-    console.log(`rowOffset: ${rowOffset}`);
-    console.log(`columnOffset : ${columnOffset}`);
+    //console.log(`rowOffset: ${rowOffset}`);
+    //console.log(`columnOffset : ${columnOffset}`);
 
     //column読み込み
-    const sheetColumn = ss.getSheetByName(columnsSheet);
-    const cols = new Columns(sheetColumn);
+    const sheetColumn = ss.getSheetByName(columnsSheet)
+    if (sheetColumn === null) return 'ColumnSheet not Found'
+    const cols = new Columns(sheetColumn)
 
-    returnValue = cols.validate(value[0], 0);
+    if (value === null) return 'range is empty'
+
+    returnValue = cols.validate((<any>value)[0], 0);
 
     return returnValue;
   } else {
-    return '';
+    return 'range is empty';
   }
 }
 
@@ -364,11 +372,12 @@ function saveAsCSVRange(rowRangeExpression: String): string {
   //settings
   const ss = SpreadsheetApp.getActiveSpreadsheet();
   readConfig();
-  console.log(`rowOffset: ${rowOffset}`);
-  console.log(`columnOffset : ${columnOffset}`);
+  //console.log(`rowOffset: ${rowOffset}`);
+  //console.log(`columnOffset : ${columnOffset}`);
 
   //column読み込み
   const sheetColumn = ss.getSheetByName(columnsSheet);
+  if (sheetColumn === null) return ''
   const cols = new Columns(sheetColumn);
 
   //sheet
@@ -390,6 +399,6 @@ function saveAsCSVRange(rowRangeExpression: String): string {
   const file = folder.createFile(fileName, <string>csvFile);
   //File downlaod
   const url = file.getUrl();
-  console.log(`${file.getUrl()}`);
+  //console.log(`${file.getUrl()}`);
   return url;
 }
